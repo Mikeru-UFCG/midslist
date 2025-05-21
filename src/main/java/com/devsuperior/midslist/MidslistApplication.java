be5cbe8a3f1dd4eb613b54e863f2d1d950e668df
Mikeru-UFCG <miguel_macedo18@hotmail.com>
2025-05-21 05:45:30 -0300

    Project created

---
+++ src/main/java/com/devsuperior/midslist/MidslistApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.midslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidslistApplication.class, args);
+	}
+
+}
