55ab2d4a6242d6544328d4330a9ab99197e99601
Mikeru-UFCG <miguel_macedo18@hotmail.com>
2025-05-22 05:55:57 -0300

    Feat: GET game by id

--- src/main/java/com/devsuperior/midslist/services/GameService.java
+++ src/main/java/com/devsuperior/midslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.devsuperior.midslist.services;
 
+import com.devsuperior.midslist.dto.GameDTO;
 import com.devsuperior.midslist.dto.GameMinDTO;
 import com.devsuperior.midslist.entities.Game;
 import com.devsuperior.midslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +16,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long listId) {
+        Game result = gameRepository.findById(listId).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
