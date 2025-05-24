ed7214041348ef4a5f686f8002033f2df5a73b9b
Mikeru-UFCG <miguel_macedo18@hotmail.com>
2025-05-24 16:56:05 -0300

    Fix: Deploy ready

--- src/main/java/com/devsuperior/midslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/midslist/controllers/GameController.java
@@ -3,7 +3,6 @@ package com.devsuperior.midslist.controllers;
 import java.util.List;
 
 import com.devsuperior.midslist.dto.GameDTO;
-import com.devsuperior.midslist.entities.Game;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
